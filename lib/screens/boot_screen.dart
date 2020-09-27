da43dc67cd48e694aa32cbdc12a0106747a143d9
Enzo Danjour <enzo.lops999@gmail.com>
2020-09-27 17:45:24 -0300

    Add boot screen

---
+++ lib/screens/boot_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BootScreen extends StatefulWidget {
+  BootScreen({Key key}) : super(key: key);
+
+  @override
+  _BootScreenState createState() => _BootScreenState();
+}
+
+class _BootScreenState extends State<BootScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       
+    );
+  }
+}
