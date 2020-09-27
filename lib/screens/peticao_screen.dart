0593caf72314415c365fa2355b8ac4bac1b7f3ff
Enzo Danjour <enzo.lops999@gmail.com>
2020-09-27 19:00:13 -0300

    Add new settings

--- lib/screens/peticao_screen.dart
+++ lib/screens/peticao_screen.dart
@@ -8,16 +8,33 @@ class PeticaoScreen extends StatefulWidget {
 }
 
 class _PeticaoScreenState extends State<PeticaoScreen> {
+  String dropdownValue = 'Escolha o tipo de causa';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(
+              right: 10,
+            ),
+            child: IconButton(
+              icon: Icon(
+                Icons.clear,
+                color: Colors.grey,
+                size: 40,
+              ),
+              onPressed: null,
+            ),
+          )
+        ],
+        centerTitle: true,
         title: Text(
-          'Contatos', 
+          'Petições',
           style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.w300,
-            fontSize: 26  
+            color: Colors.blue,
+            fontWeight: FontWeight.w400,
+            fontSize: 26,
           ),
         ),
         backgroundColor: Colors.white,
@@ -26,19 +43,56 @@ class _PeticaoScreenState extends State<PeticaoScreen> {
       body: Column(
         mainAxisSize: MainAxisSize.max,
         children: [
-          Center(
-            child: TextFormField(
-              decoration: const InputDecoration(
-                hintText: 'Qual é o título?'
+          TextFormField(
+            textAlign: TextAlign.center,
+            decoration: const InputDecoration(
+              hintText: 'Qual é o título?',
+              contentPadding: const EdgeInsets.symmetric(
+                vertical: 20,
               ),
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Por favor preencha o título';
-                }
-                return null;
-              },
             ),
-          )
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Por favor preencha o título';
+              }
+              return null;
+            },
+          ),
+          DropdownButton<String>(
+            value: dropdownValue,
+            icon: Icon(Icons.arrow_downward),
+            iconSize: 24,
+            elevation: 16,
+            style: TextStyle(color: Colors.blue),
+            onChanged: (String newValue) {
+              setState(() {
+                dropdownValue = newValue;
+              });
+            },
+            items: <String>['Escolha o tipo de causa', 'Two', 'Free', 'Four']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 10,
+              bottom: 15,
+            ),
+            child: Text(
+              'Descreva o problema:',
+            ),
+          ),
+          TextField(
+            keyboardType: TextInputType.multiline,
+            autocorrect: true,
+            autofocus: true,
+            minLines: 3,
+            maxLines: null,
+          ),
         ],
       ),
     );
