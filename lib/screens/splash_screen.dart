f04274ddf571e030861893d99cc73c1d88c13822
Enzo Danjour <enzo.lops999@gmail.com>
2020-09-27 17:48:09 -0300

    Add splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,34 @@
+import 'package:advo_help/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  static const id = 'SplashScreen';
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  void initState(){
+    super.initState();
+    pushHome();
+  }
+
+  void pushHome(){
+    Future.delayed(
+      Duration(seconds: 2),
+      () => Navigator.pushNamed(context, LoginScreen.id)
+    );
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Image.asset(
+          'images/logo_background.png',
+          fit: BoxFit.contain,
+          )
+    );
+  }
+}
