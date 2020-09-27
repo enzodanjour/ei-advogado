30e458f4943dacb7b3566eb4ce0f5a8aaaab70b5
Enzo Danjour <enzo.lops999@gmail.com>
2020-09-27 17:44:50 -0300

    Add models to navigation bar

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,61 @@
+import 'package:advo_help/components/navigation_bar.dart';
+import 'package:advo_help/components/peticao_card.dart';
+import 'package:advo_help/screens/boot_screen.dart';
+import 'package:advo_help/screens/contact_screen.dart';
+import 'package:advo_help/screens/peticao_screen.dart';
+import 'package:advo_help/services/database_firebase_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class HomeScreen extends StatefulWidget {
+  static const id = 'HomeScreen';
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  List<Widget> _children = [
+    HomePage(),
+    ContactScreen(),
+    PeticaoScreen(),
+    BootScreen(),
+  ];
+
+  void onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _children[_selectedIndex],
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _selectedIndex,
+        onItemTapped: onItemTapped,
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<QuerySnapshot>.value(
+      value: DatabaseFirebaseService().peticoes,
+      child: Container(
+        color: Colors.grey,
+        child: PeticaoCard(),
+      ),
+    );
+  }
+}
+
+
